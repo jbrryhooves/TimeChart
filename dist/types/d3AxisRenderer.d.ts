262f065a4e54d8f8a0a5379b229b941aba63d078
GitHub Action <github-action@huww98.cn>
2020-10-25 03:43:45 +0000

    New build

--- dist/types/d3AxisRenderer.d.ts
+++ dist/types/d3AxisRenderer.d.ts
@@ -6,12 +6,8 @@ export declare class D3AxisRenderer {
     private options;
     xg: Selection<SVGGElement, unknown, null, undefined>;
     yg: Selection<SVGGElement, unknown, null, undefined>;
-    xAxis: import("d3-axis").Axis<number | {
-        valueOf(): number;
-    }>;
-    yAxis: import("d3-axis").Axis<number | {
-        valueOf(): number;
-    }>;
+    xAxis: import("d3-axis").Axis<import("d3-scale").NumberValue>;
+    yAxis: import("d3-axis").Axis<import("d3-scale").NumberValue>;
     constructor(model: RenderModel, svg: SVGSVGElement, options: ResolvedRenderOptions);
     update(): void;
     onResize(width: number, height: number): void;
